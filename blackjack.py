3596ad24ddbe2888f060affeefa6ec2604f573db
subhansajadi <133183055+subhansajadi@users.noreply.github.com>
2023-08-09 17:36:17 -0400

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,11 +1,6 @@
 from random import Random
-from itertools import combinations
-
-# When using random numbers, hardcode the seed to make results reproducible.
 
 rng = Random(12345)
-
-# Define the suits and ranks that a deck of playing cards is made of.
 
 suits = ['clubs', 'diamonds', 'hearts', 'spades']
 ranks = {'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8,
@@ -28,16 +23,16 @@ def blackjack_count_value(hand):
     value is returned as a string to distinguish between blackjack
     and 21 made with three or more cards, and whether the hand is
     soft or hard."""
-    total = 0  # Current point total of the hand
-    soft = 0   # Number of soft aces in the hand
+    total = 0 
+    soft = 0  
     for (rank, _) in hand:
         v = ranks[rank]
         if v == 14:  # Treat every ace as 11 to begin with
             total, soft = total+11, soft+1
         else:
-            total += min(10, v)  # All face cards are treated as tens
+            total += min(10, v) 
         if total > 21:
-            if soft:  # Saved by the soft ace
+            if soft: 
                 soft, total = soft-1, total-10
             else:
                 return 'bust'
